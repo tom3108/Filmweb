e71eb572b19d5d488c6dc9889f2473fb380fe23c
tom3108 <t.madziara@gmail.com>
2020-05-27 23:22:14 +0200

    Django Models. Models fields. Admin

--- webmovies/admin.py
+++ webmovies/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Movie
 
-# Register your models here.
+#admin.site.register(Movie)
+
+@admin.register(Movie)
+class MovieAdmin (admin.ModelAdmin):
+	#fields = ["title", "year", "descript"]
+	#exclude = ["descript"]
+	#list_display = ["title", 'year', 'imdb_rating' ]
+	#list_filter = ['year']
+	search_fields = ["title"]
+
--- webmovies/models.py
+++ webmovies/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Movie (models.Model):
+	title = models.CharField(max_length=80, blank=False, unique=True)
+	year = models.PositiveSmallIntegerField(default=2000)
+	descript = models.TextField(default="")
+	premiere = models.DateField(null=True, blank=True)
+	imdb_rating = models.DecimalField(max_digits=4, decimal_places=2, null=True, blank=True)
+	poster = models.ImageField(upload_to="poster", null=True, blank=True)
+
+	def __str__(self):
+		return self.title_with_year()
+
+	def title_with_year(self):
+		return "{} ({})".format(self.title, self.year)
+
+
